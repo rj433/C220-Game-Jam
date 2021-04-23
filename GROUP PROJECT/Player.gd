3d7219307a23c01f3bdf9b7396dac5f5e389b89d
jalenslutzky2001 <jadslutz@iu.edu>
2021-04-23 13:04:15 -0400

    Save3

--- GROUP PROJECT/Player.gd
+++ GROUP PROJECT/Player.gd
@@ -22,9 +22,58 @@ func _physics_process(_delta):
 
 
 
-func _on_fallzone_body_entered(body):
+func _on_fallzone_body_entered(_body):
 	get_tree().change_scene("res://Level1.tscn")
 
 
-func _on_keydoor1_body_entered(body):
+func _on_keydoor1_body_entered(_body):
 	get_tree().change_scene("res://Keyroom1.tscn")
+
+
+func _on_Area2D_body_entered(_body):
+	get_tree().change_scene("res://Keyroom2.tscn")
+
+
+func _on_fallzone2_body_entered(_body):
+	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_leveldoor1_body_entered(_body):
+	get_tree().change_scene("res://Level2.tscn")
+
+
+func _on_leveldoor2_body_entered(body):
+	get_tree().change_scene("res://Level3.tscn")
+
+
+func _on_backdoor1_body_entered(body):
+	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_backdoor2_body_entered(body):
+	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_backdoor3_body_entered(body):
+	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_keydoor3_body_entered(body):
+	get_tree().change_scene("res://Keyroom3.tscn")
+
+
+func _on_goback1_body_entered(body):
+	get_tree().change_scene("res://Level1.tscn")
+
+
+
+func _on_goback2_body_entered(body):
+	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_finaldoor_body_entered(body):
+	get_tree().change_scene("res://Final Room.tscn")
+
+
+func _on_leveldoor3_body_entered(body):
+	get_tree().change_scene("res://Bossroom.tscn")
